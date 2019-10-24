134ded1d8b3fab5ea838261ab602eb9f9090a739
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2019-10-25 02:11:14 +0300

    Doléance fini

--- View/doleance.php
+++ View/doleance.php
@@ -2,111 +2,89 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="../Public/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="../Public/css/et-lineicons.css">
+	<link rel="stylesheet" href="../Public/css/font-awesome.css" />
+	<link rel="stylesheet" href="../Public/css/magnific-popup.css" />
+	<link rel="stylesheet" href="../Public/css/owl.carousel.min.css" />
+	<link rel="stylesheet" href="../Public/css/owl.theme.default.min.css" />
+	<link rel="stylesheet" href="../Public/css/aos.css" /> 
+	<link rel="stylesheet" href="../Public/css/style.css" /> 	
+	<link rel="stylesheet" href="../Public/bootstrap/css/style.css">	
+    
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/contact.js"></script>
-    <link rel="stylesheet" href="Public/Css/style.css">
+    
     <title>Doléance</title>
   </head>
   <body>
 
-    <!-- https://mdbootstrap.com/docs/jquery/forms/contact/ -->
-    <section class="mb-4">
-
-      <!--Section heading-->
-      <h2 class="h1-responsive font-weight-bold text-center my-4">Doléance</h2>
-      <!--Section description-->
-      <p class="text-center w-responsive mx-auto mb-5">Vous pouvez nous informer en cas de probléme ou de demande</p>
-
-      <div class="row">
-
-          <!--Grid column-->
-          <div class="col-md-9 mb-md-0 mb-5">
-              <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-
-                  <!--Grid row-->
-                  <div class="row">
-
-                      <!--Grid column-->
-                      <div class="col-md-6">
-                          <div class="md-form mb-0">
-                              <input type="text" id="name" name="name" class="form-control">
-                              <label for="name" class="">Prénom</label>
-                          </div>
-                      </div>
-                      <!--Grid column-->
-
-                      <!--Grid column-->
-                      <div class="col-md-6">
-                          <div class="md-form mb-0">
-                              <input type="text" id="email" name="numero" class="form-control">
-                              <label for="email" class="">Numéro</label>
-                          </div>
-                      </div>
-                      <!--Grid column-->
-
-                  </div>
-                  <!--Grid row-->
-
-                  <!--Grid row-->
-                  <div class="row">
-                      <div class="col-md-12">
-                          <div class="md-form mb-0">
-                              <input type="text" id="subject" name="subject" class="form-control">
-                              <label for="subject" class="">Subject</label>
-                          </div>
-                      </div>
-                  </div>
-                  <!--Grid row-->
-
-                  <!--Grid row-->
-                  <div class="row">
-
-                      <!--Grid column-->
-                      <div class="col-md-12">
-
-                          <div class="md-form">
-                              <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                              <label for="message">Message</label>
-                          </div>
-
-                      </div>
-                  </div>
-                  <!--Grid row-->
-
-              </form>
-
-              <div class="text-center text-md-left">
-                  <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Envoyer</a>
-              </div>
-              <div class="status"></div>
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-md-3 text-center">
-              <ul class="list-unstyled mb-0">
-                  <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                      <p>SESAME Ambatoroaka</p>
-                  </li>
-
-                  <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                      <p>Educateur AP: 034 55 555 55</p>
-                  </li>
-
-                  <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                      <p>nom@gmail.com</p>
-                  </li>
-              </ul>
-          </div>
-          <!--Grid column-->
-
-      </div>
-
-    </section>
+    <section class="contact section-padding" data-scroll-index="7">
+            <div class="container">
+                <div class="row">
+                    <div class="section-head-green col-md-12">
+                        <h4>Doléance</h4>
+                        <p class="text-center w-responsive mx-auto mb-5">Vous pouvez nous informer en cas de probléme ou de demande</p>
+                    </div>
+                    <div class="offset-md-1 col-md-10">
+                        <div class="contact-info mb-md50">
+                            <div class="row">
+                                <div class="col-md-4 mb-50">
+                                    <div class="item"> <span class="icon et-phone"></span>
+                                        <div class="cont">
+                                            <p>034 55 555 55</p>
+                                            <h6>Educateur AP</h6> </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 mb-50">
+                                    <div class="item"> <span class="icon et-envelope"></span>
+                                        <div class="cont">
+                                            <p>demo@example.com</p>
+                                            <h6>Email</h6> </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="item"> <span class="icon et-map"></span>
+                                        <div class="cont">
+                                            <p>SESAME</p>
+                                            <h6>Ambatoroka</h6> </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <form class="form" id="contact-form">
+                            <div class="messages"></div>
+                            <div class="controls">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input id="form_name" type="text" name="name" placeholder="Name" required="required"> </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input id="form_email" type="email" name="email" placeholder="Email" required="required"> </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <input id="form_subject" type="text" name="subject" placeholder="Subject"> </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <textarea id="form_message" name="message" placeholder="Message" rows="4" required="required"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 text-center">
+                                        <span class="buton buton-bg">Envoyer</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     <!--Section: Contact v.2-->
   </body>
 </html>
